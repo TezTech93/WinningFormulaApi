09ca4f957aa1e3a4977c90ab105b3415f7f736fe
TezTech93 <Shawnteztech93@gmail.com>
2026-06-30 02:24:19 -0500

    updated app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 # app.py
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from fastapi import FastAPI, HTTPException, Query, BackgroundTasks
 from fastapi.middleware.cors import CORSMiddleware
 import logging
