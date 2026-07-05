225c750182cef82575194a8f3a99ec265375761c
TezTech93 <Shawnteztech93@gmail.com>
2026-07-05 14:49:05 -0500

    updated routers init

--- routers/__init__.py
+++ routers/__init__.py
@@ -4,11 +4,15 @@ from routers.users import router as users_router
 from routers.formulas import router as formulas_router
 from routers.gamelines import router as gamelines_router
 from routers.stats import router as stats_router
+from routers.parlays import router as parlays_router
+from routers.strategies import router as strategies_router
 
 __all__ = [
     'auth_router',
     'users_router',
     'formulas_router',
     'gamelines_router',
-    'stats_router'
+    'stats_router',
+    'parlays_router',
+    'strategies_router'
 ]
