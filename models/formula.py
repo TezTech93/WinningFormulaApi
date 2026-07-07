d9b7895446590077431f28242cde6f50b5e23ecf
TezTech93 <Shawnteztech93@gmail.com>
2026-07-07 13:45:50 -0500

    Added Enum to models

--- models/formula.py
+++ models/formula.py
@@ -1,5 +1,5 @@
 # models/formula.py
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Text, Enum, Float
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Text, Enum, Float,Enum
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 import enum
