8383d0fe615dc8f63ab8af82e330e18e89384207
TezTech93 <Shawnteztech93@gmail.com>
2026-07-10 05:32:02 -0500

    fixed sports init

--- Sports/__init__.py
+++ Sports/__init__.py
@@ -1,5 +1,25 @@
-# sports/__init__.py
-from Sports.manager import SportsManager, get_sport_instance
-from Sports.base_sport import BaseSport
+from Sports.manager import SportsManager, sports_manager
+from Sports.base_scraper import BaseSportScraper
 
-__all__ = ['SportsManager', 'get_sport_instance', 'BaseSport']
+from Sports.nfl.scraper import NFLScraper
+from Sports.nba.scraper import NBAScraper
+from Sports.mlb.scraper import MLBScraper
+from Sports.nhl.scraper import NHLScraper
+from Sports.ncaaf.scraper import NCAAFScraper
+from Sports.ncaab.scraper import NCAABScraper
+
+__all__ = [
+    'SportsManager',
+    'sports_manager',
+    'BaseSportScraper',
+    'NFLScraper',
+    'NBAScraper',
+    'MLBScraper',
+    'NHLScraper',
+    'NCAAFScraper',
+    'NCAABScraper',
+    'SUPPORTED_SPORTS'
+]
+
+# Convenience variable
+SUPPORTED_SPORTS = ['nfl', 'nba', 'mlb', 'nhl', 'ncaaf', 'ncaab']
