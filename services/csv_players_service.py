a822275d484996a04afabf3aba616fc75a8a44ae
TezTech93 <Shawnteztech93@gmail.com>
2026-08-29 17:06:09 -0500

    fixed csv path

--- services/csv_players_service.py
+++ services/csv_players_service.py
@@ -1,7 +1,7 @@
 import csv
 from pathlib import Path
 
-CSV_PATH = Path("data/nfl_players.csv")
+CSV_PATH = Path("data/nfl/nfl_players.csv")
 
 def get_all_nfl_players_from_csv():
     with open(CSV_PATH, "r") as f:
